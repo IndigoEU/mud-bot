405be3e683fcb7596bcb042d96a280557f141ae7
Mavinus <mavinus77@gmail.com>
2019-06-25 18:35:28 +0200

    Alternator

--- src/main/java/com/indigo/mudbot/Functions/Alternator.java
+++ src/main/java/com/indigo/mudbot/Functions/Alternator.java
@@ -1,14 +1,29 @@
-package com.indigo.mudbot.Functions;
+package com.example.hello;
 
 public class Alternator {
     public static String owoify(String toOwO) {
-        String bigowo = toOwO.replaceAll("[n+a]", "nya")
-                .replaceAll("[n+e]", "nye")
-                .replaceAll("[n+a]", "nya");
-
-
-
-
+        String bigowo = toOwO
+                .replaceAll("na", "nya")
+                .replaceAll("for", "fow")
+                .replaceAll("oo", "owo")
+                .replaceAll("fr", "fw")
+                .replaceAll("or", "ow")
+                .replaceAll("ull", "uww")
+                .replaceAll("ur", "uw")
+                .replaceAll("oy", "oi")
+                .replaceAll("ght$", "te")
+                .replaceAll("\\ban\\b", "a")
+                .replaceAll("ua", "wa")
+                .replaceAll("er", "ew")
+                .replaceAll("\\bth\\b", "f")
+                .replaceAll("re", "we")
+                .replaceAll("tr", "tw")
+                .replaceAll("ll", "ww")
+                .replaceAll("lo", "wo")
+                .replaceAll("ra", "wa")
+                .replaceAll("le", "we")
+                .replaceAll("ro", "wo")
+                .replaceAll("v", "f");
 
 
         return bigowo;
