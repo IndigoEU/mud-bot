fe1a730839224310a55ad980342a46b1002488a6
heintai <52170808+heintai@users.noreply.github.com>
2019-06-25 18:58:27 +0100

    Create Torch.java

---
+++ src/main/java/com/indigo/mudbot/Weapons/Torch.java
@@ -0,0 +1,11 @@
+package com.indigo.mudbot.Weapons;
+public class Torch extends WeaponsTemplate {
+public Torch (){
+this.name = "Torch";
+this.assetName = "torch";
+this.attack = 100;
+this.rarity = 3; 
+this.price = 70;
+this.accuracy = 85;
+          }
+}
