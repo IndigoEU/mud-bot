af1e9c9b75286304006e859417837976b43b5449
heintai <52170808+heintai@users.noreply.github.com>
2019-06-26 05:42:53 +0100

    Create Scythe.java

---
+++ src/main/java/com/indigo/mudbot/Weapons/Scythe.java
@@ -0,0 +1,10 @@
+public class Scythe extends WeaponsTemplate {
+public Scythe (){
+this.name = "Scythe";
+this.assetName = "scythe";
+this.attack = 500;
+this.rarity = 4; 
+this.price = 350;
+this.accuracy = 80;
+          }
+}
