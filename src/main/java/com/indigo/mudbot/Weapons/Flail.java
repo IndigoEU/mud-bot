2a17a00ee38c4ed4b53cad31e02d239a683aa944
heintai <52170808+heintai@users.noreply.github.com>
2019-06-26 05:47:44 +0100

    Create Flail.java

---
+++ src/main/java/com/indigo/mudbot/Weapons/Flail.java
@@ -0,0 +1,11 @@
+package com.indigo.mudbot.Weapons;
+public class Flail extends WeaponsTemplate {
+public Flail (){
+this.name = "Flail";
+this.assetName = "flail";
+this.attack = 40;
+this.rarity = 2; 
+this.price = 60;
+this.accuracy = 70;
+          }
+}-
