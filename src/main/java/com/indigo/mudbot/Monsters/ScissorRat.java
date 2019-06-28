ced15755934772e836d51b33bdea128b57920572
IndigoEU <dragioeu@gmail.com>
2019-06-28 15:50:54 +0200

    Bugfixes

---
+++ src/main/java/com/indigo/mudbot/Monsters/ScissorRat.java
@@ -0,0 +1,12 @@
+package com.indigo.mudbot.Monsters;
+
+public class ScissorRat extends Monster {
+    public ScissorRat(){
+        this.name = "Rat with Scissors";
+        this.assetName = "scissorrat";
+        this.maxHP = 80;
+        this.damage = 20;
+        this.rewardXP = 150;
+        this.spawnChance = 2;
+    }
+}
